d29afe0d150c0fca85de8337cb0aaffac0100e40
Rapidd <rapidwolf24@gmail.com>
2015-06-02 10:46:45 +0100

    Added in base line of ban command
    added in base line of command
    got really bored

--- Documents/GitHub/chatManager/src/main/java/me/rapidw/serverEssentials/BanManager/Commands/BanExecutor.java
+++ Documents/GitHub/chatManager/src/main/java/me/rapidw/serverEssentials/BanManager/Commands/BanExecutor.java
@@ -1,7 +1,19 @@
 package me.rapidw.serverEssentials.BanManager.Commands;
 
-public class BanExecutor {
-  /*
-  *Wait for github to work
-  /*
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class BanExecutor implements CommandExecutor {
+  
+    public boolean onCommand(CommandSender sender, Command cmd, String label, args[]) {
+          Player p = Bukkit.getServer().getPlayer();
+            if (p.hasPermission("serverEssentials.banplayers");
+        if(cmd.getName().equalsIgnoreCase("banp");
+            
+          else {
+            p.sendMessage(ChatColor.RED + "I do not understand that command.")
+          }
+    }
 }
